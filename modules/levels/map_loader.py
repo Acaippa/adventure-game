d22e5523ba4b4d1ee30f72558b9649311e3a7b8b
Nils <olivernils8@gmail.com>
2022-12-02 20:17:31 +0100

    Fixed map loading function and changed to numerical naming for tiles

--- modules/levels/map_loader.py
+++ modules/levels/map_loader.py
@@ -3,14 +3,15 @@ from modules.entities.entities import*
 class MapLoader:
 	def __init__(self, level):
 		self.level = level
-		self.entity_list = {"p" : Player, "e t" : Tree}
+		self.entity_list = {"p" : Player, "1" : Tree, "0" : Entity}
 
 	def load_map(self, map): # Loops through any two-dimentional python list and initiate the different entities.
 		for y in range(len(map)):
-			for x in range(len(map)):
+			for x in range(len(map[y])):
 				type = map[y][x]
 				if type == "p":
 					self.level.player = self.entity_list[type](self.level)
-				elif type.split(" ")[0] == "e":
+
+				elif type != "0":
 					self.level.entity_list.append(self.entity_list[type](self.level, (x * 16, y * 16)))
 
